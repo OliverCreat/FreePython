a97e34acec83957c0311e91fd2ba79fffddbd0e5
OliverCreat <oliveirapmro@gmail.com>
2025-02-09 20:28:56 -0400

    first code

---
+++ teste.py
@@ -0,0 +1,12 @@
+age = 20
+firstName = "John"
+lastName = "Smith"
+newPatient = True
+
+print(firstName, lastName, "tem ", age, " anos")
+if (newPatient):
+    print(firstName, "é novo paciente")
+else:
+    print(firstName, "é paciente antigo")
+
+
