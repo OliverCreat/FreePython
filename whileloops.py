d9df8c853bbf31082e770e500acb4ce91d4ada4f
OliverCreat <oliveirapmro@gmail.com>
2025-02-11 16:24:38 -0400

    While Loops learn

---
+++ whileloops.py
@@ -0,0 +1,14 @@
+#while loop
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+print ("End")
+
+#while loop with user input
+num1 = input("insert a number: ")
+num1 = int(num1)
+while num1 <= 10:
+    print(num1 * '*')
+    num1 += 1
+print ("End")
